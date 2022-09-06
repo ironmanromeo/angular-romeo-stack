1abe3cb232fa664db4c32c3c71fcb1bdf74b54cf
Romeo Mariani <romeo.mariani@valxer.it>
2022-09-06 19:41:22 +0200

    mia su fabio

--- src/training.ts
+++ src/training.ts
@@ -1,5 +1,5 @@
 console.log('pippo pluto ddd pinco palla branch MAIN');
-// COMMENTO UNO E ADESSO MI INCAZZO 3
+// COMMENTO UNO E ADESSO MI INCAZZO 4
 // COMMENTO DA NUOVO BRANCH
 
 // Dovevi fare questo porca zozza
