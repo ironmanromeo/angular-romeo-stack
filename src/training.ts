2980805a25f5ea8a57fb22cfb1858a923219cd4a
Barbara <Babi@Air-di-Romeo.lan>
2022-09-06 20:26:23 +0200

    aaa

--- src/training.ts
+++ src/training.ts
@@ -1,9 +1,5 @@
 console.log('pippo pluto ddd pinco palla branch MAIN');
-<<<<<<< HEAD
-// COMMENTO UNO E ADESSO MI INCAZZO 2
-=======
 // COMMENTO UNO E ADESSO MI INCAZZO 4
->>>>>>> 1abe3cb232fa664db4c32c3c71fcb1bdf74b54cf
 // COMMENTO DA NUOVO BRANCH
 
 // Dovevi fare questo porca zozza
