da7e47adccf026a82197d12fd0d0e2ef8f5749db
Fabio Inguaggiato <fabio.inguaggiato@gmail.com>
2022-09-06 19:35:22 +0200

    Test

--- src/training.ts
+++ src/training.ts
@@ -1,5 +1,5 @@
 console.log('pippo pluto ddd pinco palla branch MAIN');
-// COMMENTO UNO E ADESSO MI INCAZZO 1
+// COMMENTO UNO E ADESSO MI INCAZZO 3
 // COMMENTO DA NUOVO BRANCH
 
 // Dovevi fare questo porca zozza
