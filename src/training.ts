c694a7e22756aac6dfa96c8d7c863aaa8535d8ef
Romeo Mariani <romeo.mariani@valxer.it>
2022-09-06 18:47:52 +0200

    maledizione

--- src/training.ts
+++ src/training.ts
@@ -1,5 +1,5 @@
 console.log('pippo pluto ddd pinco palla branch MAIN');
-// COMMENTO UNO E ADESSO MI INCAZZO
+// COMMENTO UNO E ADESSO MI INCAZZO 1
 // COMMENTO DA NUOVO BRANCH
 
 // Dovevi fare questo porca zozza
